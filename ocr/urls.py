96753978d703b1688ab04c72fc8a8ded2ca6913a
sina <sina.chaichi2020@gmail.com>
2018-11-25 23:59:15 +0330

    First commit

--- ocr/urls.py
+++ ocr/urls.py
@@ -1,4 +1,4 @@
-"""ocr URL Configuration
+"""learning_users URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from basic_app import views
 
 urlpatterns = [
+    path('',views.index,name='index'),
     path('admin/', admin.site.urls),
+    path('basic_app/',include('basic_app.urls')),
+    path('logout/',views.user_logout,name='logout'),
+    path('special/',views.special,name='special'),
 ]
--- ocr/wsgi.py
+++ ocr/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ocr project.
+WSGI config for learning_users project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
