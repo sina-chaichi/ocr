179398bbf1e78f44c2d3e3231882df6199b7c9b9
sina <sina.chaichi2020@gmail.com>
2018-12-03 21:14:30 +0330

    migrate to mysql

--- ocr/settings.py
+++ ocr/settings.py
@@ -85,10 +85,16 @@ WSGI_APPLICATION = 'ocr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ocr_db',
+        'USER': 'ocr_user',
+        'PASSWORD': 'ocr_pass',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
